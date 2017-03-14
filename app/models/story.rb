0963bea68e894672be1af6961b20bf1029a9753f
Vicente Martin <vicentemartintarud@gmail.com>
2017-03-14 00:48:58 -0300

    Cambios en modelo para requisito 1000 chars
    Se modifica la variable story_lim también cuando se edita

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,11 +1,13 @@
 class Story < ApplicationRecord
   has_many :comments
-  before_create :set_default, :if => :new_record?
+  before_create :set_lim, :if => :new_record?
+  before_update :set_lim
 
-  def set_default
+  def set_lim
+    puts "Aca estoy"
     i = 1
     words = self.body.split
-    self.story_lim ||= '' + words[0]
+    self.story_lim = '' + words[0]
     while 1
       begin
         if self.story_lim.length + words[i].length < 1000
